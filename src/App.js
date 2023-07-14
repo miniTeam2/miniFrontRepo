bd81bc5c6d2d642dcc57aac497f30f1b3ea65af6
minhyung <dup33333@hufs.ac.kr>
2023-07-14 11:02:12 +0900

    added components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import MainPage from "./pages/Main/MainPage" ;
+import DetailPage from "./pages/Detail/DetailPage"
+import LoginPage from "./pages/Login/LoginPage"
+import SearchResultPage from "./pages/SearchResult/SearchResultPage"
+import SignupPage from "./pages/Signup/SignupPage"
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <UserProvider>
+      <Routes>
+        <Route path = "/:userId" element={<MainPage/>} />
+        <Route path = "/login" element={<LoginPage/>}/>
+        <Route path = "/signup" element={<SignupPage/>}/>
+        <Route path="/Search/:searchId" element={<SearchResultPage />} />
+        <Route path="/:movieId" element={<DetailPage />} />
+        <Route path = "*" element={<Navigate to = "/login" replace = {true} />} />
+      </Routes>
+    </UserProvider>
+  </BrowserRouter>
   );
 }
 
